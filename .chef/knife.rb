06d6af545dbeba7804777d36bae6ca46cc6a3426
daniel.stadtherr <Daniel.Stadtherr@icfolson.com>
2016-06-09 13:34:15 -0500

    adding chef starter files

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "dstadtherr-test"
+client_key               "#{current_dir}/dstadtherr-test.pem"
+validation_client_name   "dcs-test-kitchen-validator"
+validation_key           "#{current_dir}/dcs-test-kitchen-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/dcs-test-kitchen"
+cookbook_path            ["#{current_dir}/../cookbooks"]
